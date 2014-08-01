9493c55df3066cf45d627188f2188511cf95eadf
Tomek Szpinda <tomasz.szpinda@oneserve.co.uk>
2014-08-01 07:34:16 +0100

    tidy up

--- api/web/users.go
+++ api/web/users.go
@@ -1,19 +1,35 @@
 package web
 
 import (
-	"github.com/tszpinda/userMgmt/api/store"
 	"net/http"
 	"net/url"
-	"log"
+
+	"github.com/tszpinda/goember"
+	"github.com/tszpinda/userMgmt/api/store"
 )
 
 type UserResource struct {
 	UserStore store.UserStore
 }
 
+func (this *UserResource) AddUser(url *url.URL, inHeaders http.Header, m map[string]*store.User) (int, http.Header, interface{}, error) {
+	user, _ := m["user"]
 
-func (this *UserResource) AddUser(url *url.URL, inHeaders http.Header, user *store.User) (int, http.Header, interface{}, error) {
-	log.Printf("user: %+v", user)
+	if valErr := em.Required("name", user.Name); valErr != nil {
+		return em.ValidationResponse(valErr)
+	}
+	if valErr := em.Required("email", user.Email); valErr != nil {
+		return em.ValidationResponse(valErr)
+	}
+	if valErr := em.Required("password", user.Password); valErr != nil {
+		return em.ValidationResponse(valErr)
+	}
+
+	_, err := this.UserStore.FindUserByEmail(user.Email)
+	if err == nil {
+		return em.ValidationResponse(em.ValidationError("email", "already exists"))
+	}
 	u := this.UserStore.AddUser(user.Email, user.Password, user.Name)
-	return 200, nil, u, nil
+	m["user"] = u
+	return 200, nil, m, nil
 }
